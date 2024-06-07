528fe401abfa8f5cf78dc46ba17c7b1907d56ccf
Artem <silovik1989@gmail.com>
2024-06-07 21:03:56 +0300

    сервер

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express"); //дістаємо експрес з package.json
 const app = express(); //присвоюємо змінній виклик експрес
+require("dotenv").config();
+const name = process.env.NAME;
+const age = process.env.AGE;
+const password = process.env.PASSWORD;
+console.log(name, age, password);
 
 // '/' - дістати щось з початкової сторінки. req i res - отримання інфи з фронту і відправляння інфи на фронт
 app.get("/", function (req, res) {
-  res.send("HEllo");
+  res.send(`Користувач ${name} має ${age} років і пароль - ${password}`);
 });
 
 app.listen(3000); // запуск сервера на порті 3000
