a4d16fa858b04da28e7a7047a5e931d0e63d6f38
Artem <silovik1989@gmail.com>
2024-06-14 21:00:25 +0300

    підключення html/ejs файлів і запуск їх (рендер)

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express"); //дістаємо експрес з package.json
 const app = express(); //присвоюємо змінній виклик експрес
 require("dotenv").config();
-const name = process.env.NAME;
-const age = process.env.AGE;
-const password = process.env.PASSWORD;
-console.log(name, age, password);
+
+app.use(express.static(__dirname + "/views"));
+
+app.engine("ejs", require("ejs").renderFile);
+app.set("view engine", "ejs");
 
 // '/' - дістати щось з початкової сторінки. req i res - отримання інфи з фронту і відправляння інфи на фронт
+
+app.get("/hello", function (req, res) {
+  res.send("hello piska");
+}); // інша сторінка серверу http://localhost:3000/hello/ // res.send - відправка повідомлення на сервер.
+
 app.get("/", function (req, res) {
-  res.send(`Користувач ${name} має ${age} років і пароль - ${password}`);
+  res.render("index"); // "/" - стартова сторінка серверу - http://localhost:3000,
+});
+
+app.get("/shliah", function (req, res) {
+  res.render("login");
 });
 
 app.listen(3000); // запуск сервера на порті 3000
-
-console.log("сервер запущено");
