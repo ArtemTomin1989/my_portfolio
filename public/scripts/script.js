bfad18cc40c48de4cc5ff4fa81dd4128cf70565b
ArtemTomin1989 <silovik1989@gmail.com>
2025-03-10 11:09:49 +0200

    refresh

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,14 +1,1 @@
-document.addEventListener("mousemove", (event) => {
-  let trail = document.createElement("div");
-  trail.classList.add("rainbow-trail");
-  document.body.appendChild(trail);
 
-  // Встановлення позиції райдужного сліду
-  trail.style.left = `${event.pageX}px`;
-  trail.style.top = `${event.pageY}px`;
-
-  // Видалення через 500 мс, щоб уникнути накопичення
-  setTimeout(() => {
-    trail.remove();
-  }, 500);
-});
